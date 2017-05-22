7913bce9293b8f7872b78f5c49fc3d139bb95d54
Max van IJsselmuiden <maxvanijsselmuiden@hotmail.com>
2017-05-22 15:23:34 +0200

    Added first HTML scaffolding.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FireLines | Concept</title>
+</head>
+<body>
+<div class="container-fluid">
+    <?php include('partials/header.php'); ?>
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-lg-3">
+            <div id="sidebar">
+                Sidebar
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-lg-9">
+            <div id="map">
+                Map
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-4">
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Map</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="col-xs-12 col-sm-4">
+        <div class="logo">
+            <h1>FireLines</h1>
+        </div>
+    </div>
+</div>

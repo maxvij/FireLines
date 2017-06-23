0434d51826d20b04dce378d87118f15655ddfc43
Max van IJsselmuiden <maxvanijsselmuiden@hotmail.com>
2017-06-23 10:50:58 +0200

    Added about page.

--- index.php
+++ index.php
@@ -2,84 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>FireLines | Concept</title>
+    <title>FireLines | Real-time fire reports in the Netherlands</title>
     <link rel="stylesheet" href="src/css/vendor.css" media="all"/>
     <link rel="stylesheet" href="src/css/style.css" media="all"/>
-    <script type="text/javascript" src="src/js/vivus.min.js"></script>
-    <script type="text/javascript" src="src/js/svg.min.js"></script>
-    <script type="text/javascript" src="src/js/nouislider.min.js"></script>
 </head>
 <body>
-<div id="tooltip">
-	<p class="title">Test</p>
-</div>
 <div class="container-fluid">
     <?php include('partials/header.php'); ?>
-    <div class="row">
-        <div class="col-md-12 col-lg-6 col-xl-4">
-            <div id="sidebar">
-                <div class="row">
-					<div class="col-12">
-						<div class="row">
-							<div class="col-12">
-								<h2>Filter options</h2>
-								<div class="row">
-									<div class="col-8">
-										<label>Amount of reports</label>
-									</div>
-									<div class="col-4">
-										<label class="value" id="amount-slider-value"></label>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-12">
-										<div id="amount-slider"></div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-8">
-										<label>Report priority</label>
-									</div>
-									<div class="col-4">
-										<label class="value" id="priority-slider-value">1-3</label>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-12">
-										<div id="priority-slider"></div>
-									</div>
-								</div>
-							</div>
-							<div class="col-12">
-								<label>Provinces</label>
-								<div id="provinces"></div>
-							</div>
-						</div>
-					</div>
-                    <div class="col-12">
-						<h2>Latest reports</h2>
-						<h4>Displaying <span id="number-of-reports"></span> reports</h4>
-						<div class="latest-reports-wrapper">
-							<ul class="list" id="latest-reports">
-							</ul>
-						</div>
-					</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<div class="panel">
+				<div class="heading">
+					<h2>About our installation</h2>
 				</div>
-            </div>
-        </div>
-        <div class="col-md-12 col-lg-6 col-xl-8">
-            <div id="map">
-                <div id="map-overlay">
-                    <object id="map-svg" type="image/svg+xml" data="src/img/nl.svg"></object>
-                </div>
-				<div id="data">
+				<div class="content">
 
 				</div>
-            </div>
-        </div>
-    </div>
+				<div class="footer">
+
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-<!--suppress BadExpressionStatementJS -->
-<script type="text/javascript" src="src/js/firelines.js"></script>
 </body>
 </html>

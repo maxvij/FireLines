b4a0b71cd82bc2941c6397dc32abe9da4469c156
Max van IJsselmuiden <maxvanijsselmuiden@hotmail.com>
2017-06-23 12:11:27 +0200

    Updated about page info.

--- about/index.php
+++ about/index.php
@@ -18,7 +18,13 @@
 					<h2>About our installation</h2>
 				</div>
 				<div class="content">
-
+					<p class="lead">
+						In the Netherlands, the fire squad is called about a hundred times every single day, if not more often. We sometimes do not fully realize of how much value firemen are to us. They’ll help you out in the middle of the night when you’re stuck in an elevator or when your frying pan catches fire. The unfortunate thing however is that a lot of those situations can be easily prevented by putting in a minimal amount of effort. That’s exactly why we made this installation and website: to spread awareness of fire hazards and appreciation for firemen and their work.
+					</p>
+					<h3>The installation</h3>
+					<p>To achieve this goal, we made an installation that visualizes the amount and location of the fire squad calls in the Netherlands in real-time. From an online database that updates directly after receiving an emergency call we download the info and convert them into usable coordinates and text. A marker then draws the location on a map of the Netherlands creating an overview of where the fire squad has been needed the past x amount of time. After an x amount of time the board will wipe itself clean and start over. On a panel beneath this the different most common causes of fire are visualized with a LED bar graph. This will create more awareness about our own share in preventing fires and what firemen have to do for us.</p>
+					<h3>The website</h3>
+					<p>The disadvantage of this installation is that it’s physically present at some place and won’t spread the message optimally. Therefore, we decided to add a website that digitally mimics the installation so everyone in the world can at any given time check the fire squad activities in the Netherlands. The upside of this website is that we could add extra info with every call, like the reason why and time it has been called in and an option to choose how much data is presented at a time. That makes it a valuable addition to our goal even when present at the installation.</p>
 				</div>
 				<div class="footer">
 
